d85514a668f20521f246fe540362df53f904e35a
rnflrnfl41 <120080932+rnflrnfl41@users.noreply.github.com>
2025-08-26 00:19:32 +0900

    feat: alias 추가 및 세팅 추가

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+   plugins: [
+    ['module-resolver', {
+      root: ['./'],
+      alias: {
+        '@': './',
+        '@screens': './app/screens',
+        '@components': './app/components',
+        '@features': './app/features',
+        '@shared': './app/shared',
+        '@store': './app/store',
+        '@assets': './assets',
+        '@constants': './app/shared/constants',
+        '@hooks': './app/hooks',
+        '@utils': './app/shared/utils',
+        '@types': './app/shared/types',
+        '@services': './app/services',
+        '@styles': './app/shared/styles',
+      }
+    }],
+  ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,25 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+    resolver: {
+    alias: {
+      '@': path.resolve(__dirname),
+      '@screens': path.resolve(__dirname, 'app/screens'),
+      '@components': path.resolve(__dirname, 'app/components'),
+      '@features': path.resolve(__dirname, 'app/features'),
+      '@shared': path.resolve(__dirname, 'app/shared'),
+      '@store': path.resolve(__dirname, 'app/store'),
+      '@assets': path.resolve(__dirname, 'assets'),
+      '@constants': path.resolve(__dirname, 'app/shared/constants'),
+      '@hooks': path.resolve(__dirname, 'app/hooks'),
+      '@utils': path.resolve(__dirname, 'app/shared/utils'),
+      '@types': path.resolve(__dirname, 'app/shared/types'),
+      '@services': path.resolve(__dirname, 'app/services'),
+      '@styles': path.resolve(__dirname, 'app/shared/styles'),
+    },
+  },
+  server: { port: 8081 },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
