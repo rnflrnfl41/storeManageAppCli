d640d8b000de05e4f6e9a4de00cce2a96e12455d
rnflrnfl41 <120080932+rnflrnfl41@users.noreply.github.com>
2025-08-28 00:34:04 +0900

    fix: userInfo redux관리에서 제거

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './authSlice';
 import loadingReducer from './loadingSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
     loading: loadingReducer,
   },
 });
